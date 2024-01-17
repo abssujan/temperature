c55ba965d7d2442c383f72afede271be5f871fae
abssujan <abdussabursujan@gmail.com>
2024-01-17 22:03:40 +0600

    temperature update

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ const loadApiKey = (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayLoadApiKey(data))
 }
+
+const displayLoadApiKey = wheather_information => {
+    console.log(wheather_information)
+    const tempeture = document.getElementById('tempeture');
+    const condition = document.getElementById('condition');
+    tempeture.innerText = `${wheather_information.main.temp}`;
+    condition.innerText = `${wheather_information.weather[0].main}`
+}
+document.getElementById('search-btn').addEventListener('click', function(){
+    const inputField = document.getElementById('input-filed');
+    const cityName = document.getElementById('cityName');
+    const city = inputField.value;
+    cityName.innerText = city;
+    loadApiKey(city)
+})
 loadApiKey('dhaka')
